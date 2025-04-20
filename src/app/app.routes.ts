a81b9d6efad60c13780f3257388106b6b9c674df
Vinayak-Gawade <vinayakgawade35@gmail.com>
2025-04-21 00:59:40 +0530

    Adding login functionality and other components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { LoginComponent  } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },  // Default route (Home Page)
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }  // Redirect invalid routes to Home
 ];
